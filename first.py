21cec3a55674648e7114ced212e06e83a8054616
BoghdadyAhmed <132693797+BoghdadyAhmed@users.noreply.github.com>
2023-08-15 09:42:42 +0000

    python

---
+++ first.py
@@ -0,0 +1,25 @@
+# print("Boghdady")
+#print(2+ 6*3)  
+#spam = 'Hello'
+#print(spam)
+#string text 
+#text = 'Goodbay'
+#print(text)
+# this program says hello and asks your for my name 
+#print('Hello worled')
+#print('What is your name?')
+#myname = input()
+#print('it is good to meet you, '+ myname)
+#print(len(myname))
+#print('what is your age?')
+#myage = input()
+#print(' you will be ' + str(int(myage)+1) + ' bin a year')
+
+    #for i in range(6):
+        
+   #print("Hello")
+   me=input()
+   print(me)
+   
+ 
+
